c85c3f7f757315695f30bea0ca9d0bfa4e230489
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-30 10:05:46 -0500

    worked on database set up

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE burgers_DB;
+USE burgers_DB;
+
+CREATE TABLE burgers (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES 
+("PLAIN OL’ HAMBURGER", false),
+("ORIGINAL CHEESEBURGER", true),
+("CLASSIC BYPASS™", false);
+
+SELECT * FROM burgers;
