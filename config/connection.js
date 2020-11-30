5c3ae9747092df2e386f053ce0c0a29cea912e05
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-30 17:19:10 -0500

    updated and added new sql user

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,12 @@ let connection;
 if (process.env.JAWSDB_URL) {
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-// connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "burgers_DB"
+connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "Burger_Eater",
+    password: "password",
+    database: "burgers_DB"
 })
 };
 
